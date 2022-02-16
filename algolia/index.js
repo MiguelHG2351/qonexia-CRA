28167c9cd85577cccb16b0bacb79f891b8f74825
Miguel Hernández Gaitan <miguelhernandezgaitan13@gmail.com>
2022-02-15 20:19:26 -0600

    admin key recreado

--- algolia/index.js
+++ algolia/index.js
@@ -5,7 +5,7 @@ const prisma = new PrismaClient();
 
 (async () => {
     const data = await prisma.phones.findMany()
-    const client = algoliasearch('5L0EKC0AON', '8ea138adf21bf37d0712cf1f39d8826a')
+    const client = algoliasearch('5L0EKC0AON', process.env.ALGOLIA_ADMIN_API_KEY)
     const index = client.initIndex('qonexia-products-index')
     try {
         await index.saveObjects([...data])
